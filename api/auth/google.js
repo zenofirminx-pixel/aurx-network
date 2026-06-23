f2c2034936cc346d7320264ba580a8e02192f4b6
zenofirminx-pixel <zenofirminx@gmail.com>
2026-06-23 01:27:38 +0100

    Delete google.js

--- api/auth/google.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const redirect = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
-    process.env.GOOGLE_CLIENT_ID
-  }&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20email%20profile`;
-
-  res.redirect(redirect);
-}
